a799d10d0e8911378b6e2d0f91a48dda33709f8d
matthieumarechal <tech@studiopepperoni.fr>
2026-03-03 15:59:08 +0100

    feat: add grid size prompt button
    - Add a settings button that asks the user for a grid size
    - Square sizing not yet adapted to maintain a square grid

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 const canva = document.querySelector("#canva");
 const clear = document.querySelector("#clear");
-
-
 const settings = document.querySelector("#settings");
 
-for (let i = 0 ; i < 256 ; i++) {
-    const square = document.createElement("div");
-    square.classList.add("square");
-    square.addEventListener("mouseover", () => {
-        square.classList.add("black");
-    });
-    canva.appendChild(square);
+// we choose to set grid size to 16 for the first print of the page
+let gridSize = 16;
+gridBuild(gridSize);
+
+settings.addEventListener("click", () => {
+   gridSize = parseInt(prompt("choose grid size (max 100)"));
+   console.log("Taille de la grille : " + gridSize);
+   gridBuild(gridSize);
+
+});
+
+function gridBuild(gridSize) {
+
+    squaresNumber = gridSize * gridSize;
+
+    for (let i = 0 ; i < squaresNumber ; i++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        square.addEventListener("mouseover", () => {
+            square.classList.add("black");
+        });
+        canva.appendChild(square);
+    }
 }
 
 // a clear button in order to remove the color to the painted squares
