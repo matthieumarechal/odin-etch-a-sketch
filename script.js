b50c238dc71b7a2ab2ad43a17750d461abb5c825
matthieumarechal <tech@studiopepperoni.fr>
2026-03-03 15:02:03 +0100

    feat: paint squares black on hover
    - Add mouseover event listener to each square
    - Squares turn black when hovered

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const canva = document.querySelector("#canva");
 
+for (let i = 0 ; i < 256 ; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    square.addEventListener("mouseover", () => {
+        square.classList.add("black");
+    });
+    canva.appendChild(square);
+}
 
 
-    for (let i = 0 ; i < 256 ; i++) {
-        const square = document.createElement("div");
-        square.classList.add("square");
-        canva.appendChild(square);
-    }
 
